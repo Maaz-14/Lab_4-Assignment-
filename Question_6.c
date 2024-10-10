faabec8cf8dd643545805a0f721b45d8d2d25dac
Maaz-14 <110842425+Maaz-14@users.noreply.github.com>
2024-10-10 21:24:08 +0500

    Update Question_6.c

--- Question_6.c
+++ Question_6.c
@@ -28,3 +28,48 @@ int main(){
 	printf("Number greater than 9");
 return 0;	
 }
+//OR
+// #include<stdio.h>
+// int main(){
+// 	int n;
+// 	printf("Input number: ");
+// 	scanf("%d",&n);
+// 	while (n<=0){
+// 		printf("Input number again: ");
+// 		scanf("%d",&n);
+// 	}
+// 	switch(n){
+// 		case(1):
+// 			printf("One");
+// 			break;
+// 		case(2):
+// 			printf("Two");
+// 			break;
+// 		case(3):
+// 			printf("Three");
+// 			break;
+// 		case(4):
+// 			printf("Four");
+// 			break;
+// 		case(5):
+// 			printf("Five");
+// 			break;
+// 		case(6):
+// 			printf("Six");
+// 			break;
+// 		case(7):
+// 			printf("Seven");
+// 			break;
+// 		case(8):
+// 			printf("Eight");
+// 			break;
+// 		case(9):
+// 			printf("Nine");
+// 			break;
+// 		default:
+// 			printf("Greator than 9");
+			
+// 	}
+// 	return 0;
+// }
+
