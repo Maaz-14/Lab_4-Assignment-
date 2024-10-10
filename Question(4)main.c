7c1b6df0cf28d8833d2791b369e6bb1d0407d871
Maaz-14 <110842425+Maaz-14@users.noreply.github.com>
2024-10-10 21:12:08 +0500

    Update Question(4)main.c

--- Question(4)main.c
+++ Question(4)main.c
@@ -1,25 +1,33 @@
 #include<stdio.h>
 int main(){
-	float amount, cost_after_discount;
-	printf("Enter the cost of items: ");
-	scanf("%f",&amount);
-
-	if(amount<2000){	
-		cost_after_discount=amount*(0.95);
+	int actual_amount;
+	float saved_amount,amount_after_discount;
+	printf("Enter the amount: ");
+	scanf("%d",&actual_amount);
+	if(actual_amount>=500 && actual_amount<2000){
+		saved_amount=actual_amount*0.05;
+		amount_after_discount=actual_amount-saved_amount;
 	}
-	else if (amount>=2000 && amount<=4000){
-		cost_after_discount=amount*(0.90);
+	else if(actual_amount>=2000 && actual_amount<4000){
+		saved_amount=actual_amount*0.10;
+		amount_after_discount=actual_amount-saved_amount;
 	}
-	else if (amount>=4000 && amount<=6000 ){
-		cost_after_discount=amount*(0.80);		
-	}                                    
-
-	else if (amount>6000){
-		cost_after_discount=amount*(0.65);
-	}                       
-	printf("%.2f is the amount after discount",cost_after_discount);
+	else if(actual_amount>=4000&&actual_amount<6000){
+		saved_amount=actual_amount*0.20;
+		amount_after_discount=actual_amount-saved_amount;
+	}
+	else if(actual_amount>=6000){
+		saved_amount=actual_amount*0.35;
+		amount_after_discount=actual_amount-saved_amount;
+	}
+	else if (actual_amount>0 && actual_amount<500){
+		printf("Not eligible for discount\n.");
+		
+	}
+	else
+	printf("Invalid Amount!\n");
+	printf("Actual amount: %d",actual_amount);
+	printf("\nSaved amount: %.2f",saved_amount);
+	printf("\nAmount after discount: %.2f",amount_after_discount);
 	return 0;
 }
-		
-	 
-	  
